746c56e307b8bfc80f60ed3a64963a313e0aa58b
jhhan <jhhan@logicalart.co.kr>
2016-10-18 17:37:33 +0900

    dd

---
+++ app/src/main/java/test/logicalart/net/androidurlchange/Main2Activity.java
@@ -0,0 +1,26 @@
+package test.logicalart.net.androidurlchange;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        MainActivity.loadUrl("http://www.google.com"); // because load url is a statci method, you can call the it from other class.
+
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/test/logicalart/net/androidurlchange/MainActivity.java
@@ -0,0 +1,45 @@
+package test.logicalart.net.androidurlchange;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    static WebView sWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        sWebView= webView ;
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("http://www.naver.com");
+
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public static void loadUrl (String Url){
+        sWebView.loadUrl("http://www.google.com");
+    }
+
+}
